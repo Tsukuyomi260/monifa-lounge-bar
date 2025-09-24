b9576f132631f29b02d2759da4d21f1e5d37a5f8
Onel <ezingodson@gmail.com>
2025-09-24 16:08:14 +0100

    correction de l'affichage page blanche en prod

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,17 @@ export default defineConfig({
       }
     })
   ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     port: 3000,
     host: true
